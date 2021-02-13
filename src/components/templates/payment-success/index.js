3debb0fcf00c962e366e6181271b5d73b47efe23
Rômulo Dusi <romulo.dusi@tivit.com>
2021-02-13 15:27:56 -0300

    Adiciona template de pagamento com sucesso

---
+++ src/components/templates/payment-success/index.js
@@ -0,0 +1,71 @@
+import React from 'react';
+import { View } from 'react-native';
+import Icon from 'react-native-vector-icons/EvilIcons';
+import PropTypes from 'prop-types';
+
+import Button from '../../atoms/button';
+import Text from '../../atoms/text';
+import Space from '../../atoms/space';
+
+const propTypes = {
+    /**
+     * Content text of button
+     */
+    text: PropTypes.string,
+    /**
+     * Callback for button onPress
+     */
+    onPress: PropTypes.func,
+    /**
+     * Boolean to disable button (default false)
+     */
+    disable: PropTypes.bool,
+};
+
+const defaultProps = {
+    onPress: () => { },
+};
+
+const PaymentSuccess = (props) => {
+    return (
+        <View style={{
+            flex: 1,
+            flexDirection: 'column',
+            justifyContent: 'space-between',
+        }}>
+            <View
+                style={{
+                    flex: 1,
+                    justifyContent: 'center',
+                    alignItems: 'center'
+                }}>
+                <View style={{width: '50%', alignItems: 'center'}}>
+                    <Text text='Sucesso!' size={24} centerText fullWidth/>
+                    <Text text='Compra concluída' size={24} centerText fullWidth/>
+                    <Space vertical={60} />
+                    <Icon name='check' size={93} color='#7BBDF5' />
+                    <Space vertical={41} />
+                    <Text
+                        text='Você receberá um email com os detalhes da sua compra.'
+                        size={14}
+                        centerText fullWidth />
+                </View>
+            </View>
+            <View style={{
+                paddingBottom: 40,
+                paddingHorizontal: 24,
+                justifyContent: 'flex-end',
+            }}>
+                <Button
+                    text="Ok"
+                    onPress={ props.onPress }
+                    style={{ width: '100%'}} />
+            </View>
+        </View> 
+    );
+}
+
+PaymentSuccess.propTypes = propTypes;
+PaymentSuccess.defaultProps = defaultProps;
+
+export default PaymentSuccess;
