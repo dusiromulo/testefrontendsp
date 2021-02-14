255066c46f76512994d5aeab33b930d9df3d8d8b
Rômulo Dusi <romulo.dusi@tivit.com>
2021-02-13 22:27:00 -0300

    Adiciona pagina de lista de cursos a comprar

---
+++ src/components/pages/courses-select-list/index.js
@@ -0,0 +1,27 @@
+import React, { useEffect } from 'react';
+import { View } from 'react-native';
+import FlexLayout from '../../atoms/flex-layout';
+
+// Components imports
+import Toolbar from '../../organisms/toolbar';
+import CoursesCarousel from '../../templates/courses-carousel';
+
+
+const CoursesSelectList = () => {
+    useEffect(() => {
+        // TODO CALL API TO GET COURSES INFO
+    }, []);
+
+    return (
+        <View style={{backgroundColor: 'white', flex: 1}}>
+            <Toolbar text='Loja virtual'/>
+            <CoursesCarousel coursesData={[
+                {title: 'Titulo', text: 'texto'},
+                {title: 'Titulo 2', text: 'texto2'},
+                {title: 'Titulo 3', text: 'texto3 '},
+            ]}/>
+        </View>
+    )
+}
+
+export default CoursesSelectList;
