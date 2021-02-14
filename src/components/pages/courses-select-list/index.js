2e1a0549a89a8aaa7e02f5b131c472f0b0418445
Rômulo Dusi <romulo.dusi@tivit.com>
2021-02-14 12:28:54 -0300

    Cria páginas de lista de cursos, pagamento de curso e pagamento sucesso

--- src/components/pages/courses-select-list/index.js
+++ src/components/pages/courses-select-list/index.js
@@ -1,6 +1,5 @@
 import React, { useEffect } from 'react';
 import { View } from 'react-native';
-import FlexLayout from '../../atoms/flex-layout';
 
 // Components imports
 import Toolbar from '../../organisms/toolbar';
