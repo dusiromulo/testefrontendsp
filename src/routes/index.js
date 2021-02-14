59f4ff9cd43ec64d3601087f57f416318645fa79
Rômulo Dusi <romulo.dusi@tivit.com>
2021-02-14 12:29:11 -0300

    Cria rotas

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,15 +2,17 @@ import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 
 import CoursesSelectList from '../components/pages/courses-select-list';
+import CoursePayment from '../components/pages/course-payment';
+import CoursePaymentSuccess from '../components/pages/course-payment-success';
 
 const Stack = createStackNavigator();
 
 const Routes = () => {
     return (
-        <Stack.Navigator initialRouteName="CoursesSelectList" headerMode='none'>
+        <Stack.Navigator initialRouteName="CoursePayment" headerMode='none'>
             <Stack.Screen name="CoursesSelectList" component={CoursesSelectList} />
-            {/* <Stack.Screen name="CoursePayment" component={DetailsScreen} />
-            <Stack.Screen name="CoursePaymentSuccess" component={DetailsScreen} /> */}
+            <Stack.Screen name="CoursePayment" component={CoursePayment} />
+            <Stack.Screen name="CoursesPaymentSuccess" component={CoursePaymentSuccess} />
         </Stack.Navigator>
     )
 }
